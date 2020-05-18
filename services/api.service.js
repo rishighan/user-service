01906ebb8ed5d352ef0c7d8f68192ae6efbb3f47
Rishi Ghan <rishi.ghan@gmail.com>
2020-05-18 15:42:42 -0700

    1. Adding changes to CORS policy 2. Added auth method

--- services/api.service.js
+++ services/api.service.js
@@ -1,6 +1,8 @@
 "use strict";
-
 const ApiGateway = require("moleculer-web");
+const { UnAuthorizedError } = ApiGateway.Errors;
+const _ = require("lodash");
+
 
 module.exports = {
 	name: "api",
@@ -8,15 +10,22 @@ module.exports = {
 	settings: {
 		port: process.env.PORT || 3456,
 		ip: "0.0.0.0",
-		use: [
-		],
+		use: [],
+		cors: {
+			origin: "*",
+			methods: ["GET", "OPTIONS", "POST", "PUT", "DELETE"],
+			allowedHeaders: [],
+			exposedHeaders: [],
+			credentials: false,
+			maxAge: 3600
+		},
 		routes: [
 			{
 				path: "/api",
 				whitelist: ["**"],
 				mergeParams: true,
 				authentication: false,
-				authorization: false,
+				authorization: true,
 				autoAliases: false,
 
 				callingOptions: {},
@@ -36,25 +45,68 @@ module.exports = {
 
 				// Enable/disable logging
 				logging: true,
-				
 			},
 		],
 		aliases: {
 			"POST health": "$node.health",
-			"POST /user/register": "user.register",	
+			"POST /user/register": "user.register",
 			"POST login": "user.login",
 			"GET /user/me": "user.me",
 			"PUT /user/update": "user.updateMyself",
 			"GET logout": "user.logout",
 		},
-		
 
 		log4XXResponses: false,
 		logRequestParams: null,
 		logResponseData: null,
 	},
 
-	methods: {},
+	methods: {
+		/**
+		 * Authorize the request
+		 *
+		 * @param {Context} ctx
+		 * @param {Object} route
+		 * @param {IncomingRequest} req
+		 * @returns {Promise}
+		 */
+		async authorize(ctx, route, req) {
+			let token;
+			if (req.headers.authorization) {
+				let type = req.headers.authorization.split(" ")[0];
+				if (type === "Token" || type === "Bearer")
+					token = req.headers.authorization.split(" ")[1];
+			}
+
+			let user;
+			if (token) {
+				// Verify JWT token
+				try {
+					user = await ctx.call("user.resolveToken", { token });
+					console.log(user);
+					if (user) {
+						this.logger.info(
+							"Authenticated via JWT: ",
+							user.username
+						);
+						// Reduce user fields (it will be transferred to other nodes)
+						ctx.meta.user = _.pick(user, [
+							"_id",
+							"username",
+							"email",
+						]);
+						ctx.meta.token = token;
+						ctx.meta.userID = user._id;
+					}
+				} catch (err) {
+					// Ignored because we continue processing if user doesn't exists
+				}
+			}
+
+			if (req.$action.auth == "required" && !user)
+				throw new UnAuthorizedError();
+		},
+	},
 
 	created() {},
 
